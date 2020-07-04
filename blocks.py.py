075c11087e670438e250b92f0956f16be47accfd
ando818 <andreismailyan@gmail.com>
2020-07-04 15:36:56 -0700

    Moving screen blocks synced to music

---
+++ blocks.py.py
@@ -0,0 +1,104 @@
+from tkinter import *
+import random
+import time
+colors = ['snow','RoyalBlue1', 'RoyalBlue2', 'RoyalBlue3', 'RoyalBlue4', 'blue2', 'blue4',
+    'DodgerBlue2', 'DodgerBlue3', 'DodgerBlue4', 'SteelBlue1', 'SteelBlue2',
+    'SteelBlue3', 'SteelBlue4', 'DeepSkyBlue2', 'DeepSkyBlue3', 'DeepSkyBlue4',
+    'SkyBlue1', 'SkyBlue2', 'SkyBlue3', 'SkyBlue4', 'LightSkyBlue1', 'LightSkyBlue2',
+    'LightSkyBlue3', 'LightSkyBlue4', 'SlateGray1', 'SlateGray2', 'SlateGray3',
+    'SlateGray4', 'LightSteelBlue1', 'LightSteelBlue2', 'LightSteelBlue3',
+    'LightSteelBlue4', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4',
+    'LightCyan2', 'LightCyan3', 'LightCyan4', 'PaleTurquoise1', 'PaleTurquoise2',
+    'PaleTurquoise3', 'PaleTurquoise4', 'CadetBlue1', 'CadetBlue2', 'CadetBlue3',
+    'CadetBlue4', 'turquoise1', 'turquoise2', 'turquoise3', 'turquoise4', 'cyan2', 'cyan3',
+    'cyan4', 'DarkSlateGray1', 'DarkSlateGray2', 'DarkSlateGray3', 'DarkSlateGray4',
+    'aquamarine2', 'aquamarine4', 'DarkSeaGreen1', 'DarkSeaGreen2', 'DarkSeaGreen3',
+    'DarkSeaGreen4', 'SeaGreen1', 'SeaGreen2', 'SeaGreen3', 'PaleGreen1', 'PaleGreen2',
+    'PaleGreen3', 'PaleGreen4', 'SpringGreen2', 'SpringGreen3', 'SpringGreen4',
+    'green2', 'green3', 'green4', 'chartreuse2', 'chartreuse3', 'chartreuse4',
+    'OliveDrab1', 'OliveDrab2', 'OliveDrab4', 'DarkOliveGreen1', 'DarkOliveGreen2',
+    'DarkOliveGreen3', 'DarkOliveGreen4', 'khaki1', 'khaki2', 'khaki3', 'khaki4',
+    'LightGoldenrod1', 'LightGoldenrod2', 'LightGoldenrod3', 'LightGoldenrod4',
+    'LightYellow2', 'LightYellow3', 'LightYellow4', 'yellow2', 'yellow3', 'yellow4',
+    'gold2', 'gold3', 'gold4', 'goldenrod1', 'goldenrod2', 'goldenrod3', 'goldenrod4',
+    'DarkGoldenrod1', 'DarkGoldenrod2', 'DarkGoldenrod3', 'DarkGoldenrod4',
+    'RosyBrown1', 'RosyBrown2', 'RosyBrown3', 'RosyBrown4', 'IndianRed1', 'IndianRed2',
+    'IndianRed3', 'IndianRed4', 'sienna1', 'sienna2', 'sienna3', 'sienna4', 'burlywood1',
+    'burlywood2', 'burlywood3', 'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4', 'tan1',
+    'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3', 'firebrick1', 'firebrick2',
+    'firebrick3', 'firebrick4', 'brown1', 'brown2', 'brown3', 'brown4', 'salmon1', 'salmon2',
+    'salmon3', 'salmon4', 'LightSalmon2', 'LightSalmon3', 'LightSalmon4', 'orange2',
+    'orange3', 'orange4', 'DarkOrange1', 'DarkOrange2', 'DarkOrange3', 'DarkOrange4',
+    'coral1', 'coral2', 'coral3', 'coral4', 'tomato2', 'tomato3', 'tomato4', 'OrangeRed2',
+    'OrangeRed3', 'OrangeRed4', 'red2', 'red3', 'red4', 'DeepPink2', 'DeepPink3', 'DeepPink4',
+    'HotPink1', 'HotPink2', 'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4',
+    'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4', 'PaleVioletRed1',
+    'PaleVioletRed2', 'PaleVioletRed3', 'PaleVioletRed4', 'maroon1', 'maroon2',
+    'maroon3', 'maroon4', 'VioletRed1', 'VioletRed2', 'VioletRed3', 'VioletRed4',
+    'magenta2', 'magenta3', 'magenta4', 'orchid1', 'orchid2', 'orchid3', 'orchid4', 'plum1',
+    'plum2', 'plum3', 'plum4', 'MediumOrchid1', 'MediumOrchid2', 'MediumOrchid3',
+    'MediumOrchid4', 'DarkOrchid1', 'DarkOrchid2', 'DarkOrchid3', 'DarkOrchid4',
+    'purple1', 'purple2', 'purple3', 'purple4', 'MediumPurple1', 'MediumPurple2',
+    'MediumPurple3', 'MediumPurple4', 'thistle1', 'thistle2', 'thistle3', 'thistle4']
+
+
+class Bar:
+	def __init__(self, len, width, color):
+		self.len = len
+		self.width = width
+		self.color = color
+
+class Row:
+	def __init__(self, bars):
+		self.bars = bars;
+	def draw(self):
+		barStrs = []
+		for bar in self.bars:
+			barStrs.append("".join(['-' for i in range(0, bar.len)]))
+		print("|".join(barStrs))
+	def drawOnCanvas(self, startingHeight, canvas):
+		point = 0;
+		for bar in self.bars:
+			canvas.create_rectangle(point, startingHeight, point+bar.len, startingHeight+bar.width, fill=bar.color)
+			point += bar.len
+		canvas.pack()
+			
+
+def createBars(screenSize, numBars, minBarSize, maxBarSize, barWidth):
+	sizeRemain = screenSize;
+	bars = []
+	for i in range(0, numBars):
+		numRemain = numBars - i -1;
+		smallestPossible = sizeRemain - (numRemain*maxBarSize)
+		largestPossible = sizeRemain - (numRemain*minBarSize)
+		barLength = random.randint(max(smallestPossible, minBarSize), min(maxBarSize, largestPossible))
+		sizeRemain -= barLength
+		randColor = colors[random.randint(0, len(colors)-1)]
+		bars.append(Bar(barLength, barWidth, randColor))
+	return bars
+
+def genRows(numRows):
+        rows =[]
+        for i in range(0,numRows):
+                bars = createBars(2560, 8, 250, 600, 100);
+                rows.append(Row(bars))
+        return rows;
+
+def moveRows(rows):
+        rows.pop()
+        bars = createBars(2560, 8, 100, 450, 100);
+        rows.insert(0, Row(bars))
+        return rows
+
+root = Tk()
+w = Canvas(root, width=2560, height=1000)
+rrows = genRows(10)
+
+def draw():
+	startingHeight = 0;
+	for row in genRows(10):
+		row.drawOnCanvas(startingHeight, w)
+		startingHeight += 100
+	w.after(250, draw)
+
+draw()
