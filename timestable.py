2e65548e3189200b9291a04deb772f529b110fc6
ando818 <andreismailyan@gmail.com>
2020-07-19 18:19:55 -0700

    simpify the gets

--- timestable.py
+++ timestable.py
@@ -4,37 +4,42 @@ from window import Window
 from colors import *
 
 def genMolds(i):
+	if i == 1:
+		return MoldableRectangle(100, 100, 1, 1);
 	row = i
-	col = 0
+	col = 1
 	base = MoldableRectangle(100, 100, row, col);
 	orig = base
 	for j in range(0,i-1):
 		col += 1
 		orig = orig.right(MoldableRectangle(100, 100, row, col))
 	
+	row -=1
 	topBase = MoldableRectangle(100, 100, row, col);
 	topOrig = topBase
 	for j in range(0, i-2):
-		print(row)
-		topOrig = topOrig.top(MoldableRectangle(100, 100, row, col))
 		row -= 1
-
+		topOrig = topOrig.top(MoldableRectangle(100, 100, row, col))
+		
 	orig.top(topBase)
 	return base
 
 window = Window(800, 800)
 starty=0
-for i in range(1,10):
+
+for i in range(1,4):
 	rect2= genMolds(i)
 	finalMold, pos = rect2.weld()
 	window.place(Point(0,starty), finalMold)
 	starty+=100
 
-carves = window.getCarves()
+carves = window.getCarves(None, None)
 
 root = Tk()
 canvas = Canvas(root, width=window.width, height=window.height)
 window.draw(canvas)
 
-genMolds(3)
-window.getByL1(3)
+for carve in window.getCarves(1, None):
+	carve.color = 'red'
+
+window.draw(canvas)
--- window.py
+++ window.py
@@ -15,7 +15,7 @@ class Window:
 			self.carves[point].append(Carve(point, piece))
 			if (hasattr(piece, "l1")):
 				self.carvesByL1[piece.l1].append(Carve(point, piece))
-			if (hasattr(piece, "l1")):
+			if (hasattr(piece, "l2")):
 				self.carvesByL2[piece.l2].append(Carve(point, piece))
 
 	def draw(self, canvas):
@@ -29,22 +29,16 @@ class Window:
 				canvas.create_rectangle(startP.x + carveX, startP.y + carveY, startP.x + carveX + objWidth, startP.y + carveY + objHeight, fill=carve.color)
 				canvas.pack()
 
-	def getCarves(self):
-		l = []
-		for p in self.carves:
-			l.extend(self.carves[p])
-		return l
-	def getByL1(self, l1):
+	def getCarves(self, l1, l2):
 		l =[]
-		carves =  self.carvesByL1[l1]
-		for carve in carves:
-			for p in self.carves:
-				for c in self.carves[p]:
-					if (c.piece.l1 == carve.piece.l1):
-						print(c.piece.l1)
-						l.append(c)
+		for point in self.carves:
+			for carve in self.carves[point]:
+				meets = True
+				meets = True if l1 == None else carve.piece.l1 == l1 
+				meets = meets and (True if l2 == None else carve.piece.l2 == l2)
+				if (meets):
+					l.append(carve)
 		return l
-
 
 
 class Carve():
